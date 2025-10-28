1be4a404cae60891ef4785ba2d941a047de33529
Max Rojas <maxsmilegridproject@gmail.com>
2025-10-28 09:36:07 -0400

    eliminar archivos erroneos

--- inicio.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-  const encabezado = document.getElementById("encabezado");
-  if (window.scrollY > 50) {
-    encabezado.style.backgroundColor = "#ffdfc5";
-    encabezado.style.transition = "background-color 0.5s ease";
-  } else {
-    encabezado.style.backgroundColor = "#ffeedf";
-  }
-});
